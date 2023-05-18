0fedfd81a48750d63bbec1c66f5902fdd4c56581
tonykim0203 <62566613+tonykim0203@users.noreply.github.com>
2023-05-18 16:03:45 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Set constraints for the video stream
-var constraints = { video: { facingMode: "environment" }, audio: false };
+// var constraints = { video: { facingMode: "environment" }, audio: false };
+var constraints = { video: true, audio: false };
 
 // Define constants
 const cameraView = document.querySelector("#camera--view"),
